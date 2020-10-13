260643f25cf12598128abf852a07f2c23a1c0e34
DESKTOP-NBQV9PB\ADMIN <durajg@hq.empyra.com>
2020-10-14 00:01:22 +0530

    change for first page calling

--- Portal/urls/Staff.py
+++ Portal/urls/Staff.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Portal.views import Staff
 
 urlpatterns = [
-    path('details',Staff.person_details, name='Persons_details'),
+    path('',Staff.person_details, name='Persons_details'),
     path('detail/<str:pk>',Staff.person, name='Person_details'),
     path('create',Staff.create_users, name='create_users'),
     path('update/<str:pk>',Staff.update_users, name='update_users'),
--- career_service/urls.py
+++ career_service/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/details',include('Portal.urls.Staff')),
+    path('',include('Portal.urls.Staff')),
     path('',include('Portal.urls.Login'))
 ]
